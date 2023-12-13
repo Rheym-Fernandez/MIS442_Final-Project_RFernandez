7154f43f99e0fcd0275239ab327718243e9ebe7d
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-12-13 14:02:39 -0800

    Cleaned code in IngredientsTest class.

--- MIS442_Final Project_RFernandez/BitsTests/IngredientTests.cs
+++ MIS442_Final Project_RFernandez/BitsTests/IngredientTests.cs
@@ -85,33 +85,6 @@ namespace BitsTests
             }
         }
 
-
-        /* [Test]
-         public void GetByPrimaryKeyTest()
-         {
-             //This code gets the product that has a code of A4VB
-             //Assert.AreEqual("Murach's ASP.NET 4 Web Programming with VB 2010", p.Description) means that
-             //the code "A4VB" is equivalent to the product description of "Murach's ASP.NET 4 Web
-             //Programming with VB 2010"
-             p = dbContext.Products.Find("A4VB");
-             Assert.IsNotNull(p);
-             Assert.AreEqual("Murach's ASP.NET 4 Web Programming with VB 2010", p.Description);
-             Console.WriteLine(p);
-         }
-
-         [Test]
-         public void GetUsingWhere()
-         {
-             //Get a list of all of the products that have a unit price of 56.50
-             //First line gets the code of all products that have a cost of 56.50; then outputting it to a list.
-             //Based on the MYSQL query, I am asserting that there should be 7 products returned with a price of 56.5
-             products = dbContext.Products.Where(p => p.UnitPrice.Equals(56.5m)).OrderBy(p => p.ProductCode).ToList();
-             Assert.AreEqual(7, products.Count);
-             Assert.AreEqual("A4CS", products[0].ProductCode);
-             PrintAll(products);
-         }
-
-        */
         public void PrintAll(List<Ingredient> ingredients)
         {
             foreach (Ingredient i in ingredients)
